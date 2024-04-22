7d7a01dbcc62cc8cc3e5f88338729f8e6d658d25
Hugo santos <santoscostahugo5@gmail.com>
2024-04-22 00:07:56 +0000

    java script do agradecimento

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ botoes.forEach(botao =>{
     botao.addEventListener("click",guardarNota)
 })
 
-function guardarNota() {
+function guardarNota(evento) {
     let nota = evento.target.innerText
 
     localStorage.setItem("nota",nota)
